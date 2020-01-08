1c86cc1b1163d429cb7f86b5128b1e51df9df9ba
Michal Altair Valášek <michal.valasek@altairis.cz>
2020-01-08 02:55:37 +0100

    Implemented compiler tool.

--- Altairis.Tmd.Compiler/Program.cs
+++ Altairis.Tmd.Compiler/Program.cs
@@ -1,9 +1,122 @@
 ﻿using System;
+using System.IO;
+using NConsoler;
+using Altairis.Tmd.Core;
+using System.Diagnostics;
 
 namespace Altairis.Tmd.Compiler {
     class Program {
+        private const int ERRORLEVEL_SUCCESS = 0;
+        private const int ERRORLEVEL_FAILURE = 1;
+
+        private static bool debugMode;
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            Console.WriteLine($"TMDC: Tutorial Markdown Compiler version {version:4}");
+            Console.WriteLine("Copyright (c) Michal Altair Valasek - Altairis, 2019-2020");
+            Console.WriteLine("www.altairis.cz | www.rider.cz | github.com/ridercz/TMD");
+            Console.WriteLine();
+            Consolery.Run();
         }
+
+        [Action("Compile TMD file to HTML")]
+        public static void Compile(
+            [Required(Description = "Input file name")] string inputFileName,
+            [Optional(null, "o", Description = "Output file name")] string outputFileName,
+            [Optional(false, "os", Description = "Open output file in default browser")] bool startOutputFile,
+            [Optional(null, "t", Description = "Document template file name")] string templateFileName,
+            [Optional("<!--body-->", "tp", Description = "Template placeholder for body")] string templatePlaceholder,
+            [Optional(false, Description = "Show detailed exception messages")] bool debug
+            ) {
+
+            debugMode = debug;
+
+            // Read source
+            var source = TryDo(
+                $"Reading source from {inputFileName}...",
+                () => File.ReadAllText(inputFileName)
+            );
+
+            // Read template
+            var templateHtml = Properties.Resources.DefaultTemplate;
+            if (!string.IsNullOrWhiteSpace(templateFileName)) TryDo(
+                $"Reading template from {templateFileName}...",
+                () => File.ReadAllText(templateFileName)
+            );
+
+            // Compile
+            var parser = new TmdParser(renderOptions: new TmdRenderOptions {
+                InformationTemplate = "<tr class=\"information\">\r\n\t<th>&#9432;</th>\r\n\t<td>{0}</td>\r\n</tr>",
+                WarningTemplate = "<tr class=\"warning\">\r\n\t<th>&#9888;</th>\r\n\t<td>{0}</td>\r\n</tr>",
+                DownloadTemplate = "<tr class=\"download\">\r\n\t<th>&#128426;</th>\r\n\t<td>{0}</td>\r\n</tr>"
+            });
+
+            var html = TryDo(
+                "Compiling...",
+                () => parser.Render(source)
+            );
+
+            // Use template
+            html = templateHtml.Replace(templatePlaceholder, html);
+
+            // Save to output file
+            if (string.IsNullOrWhiteSpace(outputFileName)) outputFileName = Path.Combine(Path.GetDirectoryName(inputFileName), Path.GetFileNameWithoutExtension(inputFileName) + ".html");
+            TryDo(
+                $"Saving to {outputFileName}...",
+                () => File.WriteAllText(outputFileName, html)
+            );
+
+            // Start output file
+            if (startOutputFile) TryDo(
+                  "Opening output file...",
+                  () => Process.Start(new ProcessStartInfo { FileName = outputFileName, UseShellExecute = true })
+              ); ;
+
+            Environment.Exit(ERRORLEVEL_SUCCESS);
+        }
+
+        private static T TryDo<T>(string message, Func<T> func) {
+            try {
+                Console.Write(message);
+                var result = func();
+                Console.WriteLine("OK");
+                return result;
+#pragma warning disable CA1031 // Do not catch general exception types
+            } catch (Exception ex) {
+                CrashExit("FAILED: {0}", ex);
+                return default;
+            }
+#pragma warning restore CA1031 // Do not catch general exception types
+        }
+
+        private static void TryDo(string message, Action action) {
+            try {
+                Console.Write(message);
+                action();
+                Console.WriteLine("OK");
+#pragma warning disable CA1031 // Do not catch general exception types
+            } catch (Exception ex) {
+                CrashExit("FAILED: {0}", ex);
+            }
+#pragma warning restore CA1031 // Do not catch general exception types
+        }
+
+        private static void CrashExit(string message, Exception ex = null) {
+            if (ex == null) {
+                Console.Error.WriteLine(message);
+            } else {
+                Console.Error.WriteLine(message, ex.Message);
+                if (debugMode) {
+                    Console.Error.WriteLine(new string('-', Console.BufferWidth));
+                    Console.Error.WriteLine(ex.ToString());
+                    Console.Error.WriteLine(new string('-', Console.BufferWidth));
+                }
+            }
+
+            Console.Error.WriteLine("Program execution terminated.");
+            Environment.Exit(ERRORLEVEL_FAILURE);
+        }
+
     }
 }
