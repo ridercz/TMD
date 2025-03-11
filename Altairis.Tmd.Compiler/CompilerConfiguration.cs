786a8090aaf4a5b1a7a99e60f963b3d8afe9a3be
Michal Altair Valášek <github@rider.cz>
2025-03-11 20:01:49 +0100

    Remove V1 projects

--- Altairis.Tmd.Compiler/CompilerConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using Altairis.Tmd.Core;
-
-namespace Altairis.Tmd.Compiler {
-    public class CompilerConfiguration {
-        public TmdParserOptions ParserOptions { get; set; }
-
-        public TmdRenderOptions RenderOptions { get; set; }
-    }
-}
